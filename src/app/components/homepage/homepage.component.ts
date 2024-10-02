07713a16b7238550dc875b7a4e6e929f957f8660
kyra-moore21 <kyra.moore21@icloud.com>
2024-10-02 12:26:11 -0400

    added general ui for form and it's mood buttons, moved styling into tailwind.config

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonicModule, NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
+  imports: [IonicModule],
   standalone: true
 })
 export class HomepageComponent  implements OnInit {
 
-  constructor() { }
+
+  constructor(private router: Router) { }
+
 
   ngOnInit() {}
+
+
+  navigateToForm(type: 'Song' | 'Playlist'){
+    this.router.navigate(['/form'], {
+      queryParams: { type: type }
+    });
+   
+  }
+
 
 }
